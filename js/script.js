4bb383dd5e8d056ab3c9a86baa01596819d6e10a
GuilhermeAGoncalves <guilhermegpop@gmail.com>
2021-11-03 00:28:27 -0300

    adicionando empate

--- js/script.js
+++ js/script.js
@@ -71,13 +71,20 @@ let c3 = document.getElementById('C3').getAttribute('jogada')
 
 let vencedor = '';
 
-if(((a1 === b1 && a1 === c1) || (a1 === a2 && a1 === a3) || (a1 === b2 && a1 === c3)) && a1 != ''){
+if(((a1 === b1 && a1 === c1) || (a1 === a2 && a1 === a3) || (a1 === b2 && a1 === c3)) && a1 != '')
+{
     vencedor = a1;
-}else if(((b2 === b1 && b2 === b3)||(b2 === a2 && b2 === c2)||(b2 === a3 && b2 === c1 )) && b2 != ''){
+}
+else if(((b2 === b1 && b2 === b3)||(b2 === a2 && b2 === c2)||(b2 === a3 && b2 === c1 )) && b2 != ''){
     vencedor = b2;
 }
-else if(((c3 === c2 && c3 === c1 )||( c3 === a3 && c3 === b3)) && c3 != ''){
+else if(((c3 === c2 && c3 === c1 )||( c3 === a3 && c3 === b3)) && c3 != '')
+{
     vencedor = c3
+}
+else if(a1 != '' && a2 != '' && a3 != '' && b1 != '' && b2 != '' && b3 != '' && c1 != '' && c2 != '' && c3 != '' && vencedor != a1 && vencedor != b2 && vencedor != c3){
+    gameOver = true
+    alert('Deu velha')
 }
 
 if (vencedor != ''){
